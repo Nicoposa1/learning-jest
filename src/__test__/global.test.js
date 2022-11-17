58ca3f0eaac800b1fc6e6de0105adad1cc576acd
Nicolas Posa <nicolasposa@Nicolass-MacBook-Air.local>
2022-11-16 23:07:53 -0300

    test async/await

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -41,3 +41,14 @@ test("Probar una promesa", () => {
     expect(string).toBe("aloH");
   });
 });
+
+test("test async/await", async () => {
+  const string = await reverseString2("Hola");
+  expect(string).toBe("aloH")
+})
+
+afterEach(() => console.log("Despues de cada prueba"))
+afterAll(() => console.log("Despues de todas las pruebas"))
+
+beforeEach(() => console.log("Antes de cada prueba"))
+beforeAll(() => console.log("Antes de todas las pruebas"))
